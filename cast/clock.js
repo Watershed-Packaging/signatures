3aaba60b6bd34480d559ef83735adecff5c66030
Watershed-Packaging <152299615+Watershed-Packaging@users.noreply.github.com>
2024-02-22 11:58:14 +0000

    Add files via upload

--- cast/clock.js
+++ cast/clock.js
@@ -1,12 +1,9 @@
 //DATE AND HOUR
-/*const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 const d = new Date();
 let day = weekday[d.getDay()];
-document.getElementById("day1").innerHTML = day;
-document.getElementById("day2").innerHTML = day;
-document.getElementById("date1").innerHTML = d.getDate() + "." + d.getMonth() + 1 + "." + d.getFullYear();
-document.getElementById("date2").innerHTML = d.getDate() + "." + d.getMonth() + 1 + "." + d.getFullYear();*/
+document.getElementById("date").innerHTML = day + " - " + d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear();
 
 setInterval(myTimer, 1000);
 
