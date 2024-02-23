6835f80ea75ff872e65b170c37967e6d3c4971fa
Watershed-Packaging <152299615+Watershed-Packaging@users.noreply.github.com>
2024-02-23 13:42:36 +0000

    Add files via upload

--- cast/js/xmld.js
+++ cast/js/xmld.js
@@ -7,42 +7,19 @@
 
   xhttp.open("GET", "xml/guests.xml", true);
   xhttp.send();
+  
+  var x = document.getElementsByClassName("m4")
 
   function myFunction3b(xml) {	
     var xmlDoc = xml.responseXML;
     var d1 = xmlDoc.getElementsByTagName("monday");
-    document.getElementById("line1").getElementsByClassName("m4")[0].innerHTML = d1[0].childNodes[1].childNodes[1].childNodes[0].nodeValue;
-  }
-myFunction3b(xml);
-
-// Choose data from XML document.
-function myFunction2(c) {
-
-  var y = document.getElementsByTagName("button");	
-
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        myFunction3(this);
+    for (i = 0; i < x.length; i++) {
+    var y = 1;
+    document.getElementById("line1").getElementsByClassName("m4")[i].innerHTML = d1[0].childNodes[y].childNodes[1].childNodes[0].nodeValue;
+    document.getElementById("line1").getElementsByClassName("m5")[i].innerHTML = d1[0].childNodes[y].childNodes[3].childNodes[0].nodeValue;
+    document.getElementById("line1").getElementsByClassName("m2")[i].innerHTML = d1[0].childNodes[y].childNodes[5].childNodes[0].nodeValue;
+    y = y+2;
     }
-  };
-  xhttp.open("GET", "xml/guests.xml", true);
-  xhttp.send();
-	
-  function myFunction3(xml) {
-    var xmlDoc = xml.responseXML;
-    var x1 = xmlDoc.getElementsByTagName("name");
-    var x2 = xmlDoc.getElementsByTagName("position");
-    var x3 = xmlDoc.getElementsByTagName("tel");
-    var x4 = xmlDoc.getElementsByTagName("mail");
-    var w = xmlDoc.getElementsByTagName("wales");
-    for (i = 0; i < y.length; i++) {
-      var z = y[i+1]; // Because I already have one "button" element.
-      if (z.id === c) { 
-    document.getElementById("name").innerHTML = x1[i].childNodes[0].nodeValue;
-    document.getElementById("position").innerHTML = x2[i].childNodes[0].nodeValue;
-    document.getElementById("tel").innerHTML = x3[i].childNodes[0].nodeValue;
-   } 	
   }
- }
-}
+
+myFunction3b(xml);
