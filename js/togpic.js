e9c6e16b726dcf08ffd9895232910c65a54caa2e
Watershed-Packaging <152299615+Watershed-Packaging@users.noreply.github.com>
2024-03-04 08:39:24 +0000

    Add files via upload

--- js/togpic.js
+++ js/togpic.js
@@ -14,7 +14,7 @@ function swap(c) {
   var rw = "employees/assets/avatars/RebeccaWillis.png";
   var sbb = "employees/assets/avatars/SophieBullen-Brown.png";
   var wk = "employees/assets/avatars/WitoldKrzysztofek.png";
-  const pics = [logo, logo, ab, cr, logo, dw, logo, db, logo, em, logo, logo, logo, kw, logo, logo, logo, logo, logo, logo, ny, logo, logo, logo, logo, logo, rw, sbb, logo, logo, logo, logo, wk];
+  const pics = [logo, logo, ab, cr, logo, dw, logo, db, logo, logo, em, logo, logo, logo, kw, logo, logo, logo, logo, logo, logo, ny, logo, logo, logo, logo, logo, rw, sbb, logo, logo, logo, logo, wk];
   for (i = 0; i < y.length; i++) {
       var z = y[i+1]; // Because I already have one "button" element.
       if (z.id === c) {
