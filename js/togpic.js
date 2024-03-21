d4edfa622673751c2d3e07e817f2c2bc2f408671
Watershed-Packaging <152299615+Watershed-Packaging@users.noreply.github.com>
2024-03-21 15:36:35 +0000

    Add files via upload

--- js/togpic.js
+++ js/togpic.js
@@ -6,6 +6,7 @@ function swap(c) {
   var logo = "employees/assets/avatars/placeholder.png";
   var ab = "employees/assets/avatars/AnoushkaBlakeman.png";
   var cr = "employees/assets/avatars/ChrisRansome.png";
+  var ch = "employees/assets/avatars/ClaireHarrison.png";
   var dw = "employees/assets/avatars/DanielWaters.png";
   var db = "employees/assets/avatars/DavidBurton.png";
   var em = "employees/assets/avatars/EviMcNally.png";
@@ -14,7 +15,7 @@ function swap(c) {
   var rw = "employees/assets/avatars/RebeccaWillis.png";
   var sbb = "employees/assets/avatars/SophieBullen-Brown.png";
   var wk = "employees/assets/avatars/WitoldKrzysztofek.png";
-  const pics = [logo, logo, ab, cr, logo, dw, logo, db, logo, logo, em, logo, logo, logo, kw, logo, logo, logo, logo, logo, logo, ny, logo, logo, logo, logo, logo, rw, sbb, logo, logo, logo, logo, wk];
+  const pics = [logo, logo, ab, cr, ch, dw, logo, db, logo, logo, em, logo, logo, logo, kw, logo, logo, logo, logo, logo, logo, ny, logo, logo, logo, logo, logo, rw, sbb, logo, logo, logo, logo, wk];
   for (i = 0; i < y.length; i++) {
       var z = y[i+1]; // Because I already have one "button" element.
       if (z.id === c) {
