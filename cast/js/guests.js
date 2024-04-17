80e8481354dd303f5ad428cc76207b0246442690
Watershed-Packaging <152299615+Watershed-Packaging@users.noreply.github.com>
2024-04-17 15:15:10 +0100

    Update guests.js

--- cast/js/guests.js
+++ cast/js/guests.js
@@ -55,7 +55,7 @@ var d4 = [
 
 /* _______________________GUESTS___________________________________ */
 
-  "00:00 am", "Name1", "Company",
+  "10:30 am", "Robert Jones & Tom Brook", "Samuel Grant Packaging",
   "00:00 am", "Name2", "Company",
   "00:00 am", "Name3", "Company",
   "00:00 am", "Name4", "Company"
