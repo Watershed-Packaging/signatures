e62fd5bd5a946381a53e1f2e03a18ddd1f71c302
Watershed-Packaging <152299615+Watershed-Packaging@users.noreply.github.com>
2024-04-29 13:56:17 +0100

    Update guests.js

--- cast/js/guests.js
+++ cast/js/guests.js
@@ -56,7 +56,7 @@ var d4 = [
 /* _______________________GUESTS___________________________________ */
 
   "10:30 am", "Kathryn Hanson", "Avery",
-  "00:00 am", "Name2", "Company",
+  "12:00 pm", "David Brooksbank", "Zeller",
   "00:00 am", "Name3", "Company",
   "00:00 am", "Name4", "Company"
 
